264dc76dfcd8730a4465faf4cea6e6b094e3b74d
Giuseppe <giuseppe.giordano1992@gmail.com>
2016-10-26 16:01:04 +0200

    Esempio propertyplaceholder

---
+++ SrcSpringBatchCorso/Esempio/ComponentiStandardSpring/src/main/java/it/addvalue/esempio/generics/ClassToInjectFileName.java
@@ -0,0 +1,10 @@
+package it.addvalue.esempio.generics;
+
+public class ClassToInjectFileName {
+
+	private String fileName;
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
